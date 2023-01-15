d1ecdf79fb184dc018956e94a609489727a7ed8c
kamil cakir <kamilcakir88@gmail.com>
2023-01-15 14:28:54 +0300

    feat: Server setup

---
+++ server/kotlin-monorepo-server/src/test/kotlin/com/kml55/kotlinmonoreposerver/KotlinMonorepoServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kml55.kotlinmonoreposerver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinMonorepoServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
