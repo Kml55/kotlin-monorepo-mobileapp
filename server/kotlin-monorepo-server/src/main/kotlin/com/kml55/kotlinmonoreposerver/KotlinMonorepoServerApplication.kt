b3f9d161387212165d6134deb8c0b672f12955a4
kamil cakir <kamilcakir88@gmail.com>
2023-01-17 23:10:09 +0300

    feat: server moved under main folder

---
+++ server/kotlin-monorepo-server/src/main/kotlin/com/kml55/kotlinmonoreposerver/KotlinMonorepoServerApplication.kt
@@ -0,0 +1,11 @@
+package com.kml55.kotlinmonoreposerver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinMonorepoServerApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinMonorepoServerApplication>(*args)
+}
